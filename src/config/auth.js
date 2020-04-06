f09955e031e5704f18f177aaff5b0e9995e4979b
KevenJunco <49709096+KevenJunco@users.noreply.github.com>
2020-04-06 09:28:21 -0300

    add jwt
    add jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9bb5487797204db9e849b8126f336d5d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'biziga da Silva',
-    email: 'asdasd@fasfsad.com.br',
-    password_hash: 'dadsad',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
