bb0ab41dda854edc6e59182abe0541b6f5738da6
KevenJunco <49709096+KevenJunco@users.noreply.github.com>
2020-04-08 11:26:18 -0300

    Add appointment
    add apointment features

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,12 @@ import Sequelize from 'sequelize';
 // Models import
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
 // models list to insert in database
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
