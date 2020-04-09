64358713d5d1010cb20270981a48bf7404c18956
KevenJunco <49709096+KevenJunco@users.noreply.github.com>
2020-04-09 17:28:58 -0300

    add nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '15c362ff22c42c',
+    pass: '9be95a6fdda50c',
+  },
+  default: {
+    from: 'Developers Microhard <noreply@appteste.com>',
+  },
+};
